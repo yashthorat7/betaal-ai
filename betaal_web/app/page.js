6f7a64486e3d38bd1be8f8201730e3cb3553b3ed
Yash Thorat <yshthrt@gmail.com>
2026-03-02 21:35:15 +0530

    web added

---
+++ betaal_web/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Betaal AI Website</h1>
+      <p className="mt-4 text-xl">Digital Rehabilitation for Screen Addiction</p>
+    </main>
+  );
+}
